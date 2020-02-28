fe449c8c286fbbd68e7e0bb7fface2e00b5354ec
吴凯轩 <kaaaaaai6@163.com>
2020-02-28 17:05:43 +0800

    安装element-ui

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
-
+Vue.use(ElementUI) // 全局注册ElementUI组件
 new Vue({
   router,
   store,
